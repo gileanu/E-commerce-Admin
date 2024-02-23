ee3cb03f8132bd06f6006d858f475f068a6fca57
gileanu <gileanu@gmail.com>
2024-02-23 10:30:41 +0200

    [feat] completed Dashboard

--- app/(dashboard)/[storeId]/(routes)/page.tsx
+++ app/(dashboard)/[storeId]/(routes)/page.tsx
@@ -1,17 +1,72 @@
-import prismadb from "@/lib/prismadb";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { Heading } from "@/components/ui/heading";
+import { Separator } from "@/components/ui/separator";
+import { formatter } from "@/lib/utils";
+import { Box, CreditCard, DollarSign } from "lucide-react";
+import { getTotalRevenue } from "@/actions/get-total-rev";
+import { getSalesCount } from "@/actions/get-sales-count";
+import { getStockCount } from "@/actions/get-sock-count";
+import { getGraphData } from "@/actions/get-graph-data";
+import { Overview } from "@/components/overview";
 
 interface DashboardPageProps {
   params: { storeId: string };
 }
 
 const DashboardPage: React.FC<DashboardPageProps> = async ({ params }) => {
-  const store = await prismadb.store.findFirst({
-    where: {
-      id: params.storeId,
-    },
-  });
-
-  return <div>Active Store: {store?.name}</div>;
+  const totalRevenue = await getTotalRevenue(params.storeId);
+  const salesCount = await getSalesCount(params.storeId);
+  const stockCount = await getStockCount(params.storeId);
+  const graphRev = await getGraphData(params.storeId);
+  return (
+    <div className="flex-col">
+      <div className="flex-1 space-y-4 p-8 pt-6">
+        <Heading title="Dashboard" description="Overview of your store" />
+        <Separator />
+        <div className="grid gap-4 grid-cols-3">
+          <Card>
+            <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+              <CardTitle className="text-sm font-medium">
+                Total Revenue:
+              </CardTitle>
+              <DollarSign className="h-4 w-4 text-muted-foreground" />
+            </CardHeader>
+            <CardContent>
+              <div className="text-3xl font-bold">
+                {formatter.format(totalRevenue)}
+              </div>
+            </CardContent>
+          </Card>
+          <Card>
+            <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+              <CardTitle className="text-sm font-medium">Sales:</CardTitle>
+              <CreditCard className="h-4 w-4 text-muted-foreground" />
+            </CardHeader>
+            <CardContent>
+              <div className="text-3xl font-bold">+{salesCount}</div>
+            </CardContent>
+          </Card>
+          <Card>
+            <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+              <CardTitle className="text-sm font-medium">Stock:</CardTitle>
+              <Box className="h-4 w-4 text-muted-foreground" />
+            </CardHeader>
+            <CardContent>
+              <div className="text-3xl font-bold">{stockCount}</div>
+            </CardContent>
+          </Card>
+        </div>
+        <Card className="col-span-4">
+          <CardHeader>
+            <CardTitle>Overview:</CardTitle>
+          </CardHeader>
+          <CardContent className="pl-2">
+            <Overview data={graphRev} />
+          </CardContent>
+        </Card>
+      </div>
+    </div>
+  );
 };
 
 export default DashboardPage;
